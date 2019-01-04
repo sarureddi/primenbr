2433702d8e6d2e0671539a82ce6d7df437b97edc
saru <sarumathiayyappan@gmail.com>
2019-01-04 11:50:14 +0530

    Update and rename primen.c to primenbr.c

---
+++ primenbr.c
@@ -0,0 +1,18 @@
+# primenbr
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int i;
+    printf("enter the value");
+    scanf("%d",&i);
+    if(i%i==1)
+        {
+        printf("the number is prime number");
+        }
+    else
+    {
+        printf("the number is  not an prime number");
+    }
+}
+retirn 0;
